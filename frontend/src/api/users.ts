a75349b06302515c1b3f2834420c22c4a7994083
Juan Pablo Listte <juanpilistte@gmail.com>
2023-12-19 20:39:25 -0300

    fix axi post

--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -1,5 +1,5 @@
 import { axi } from './useAxios';
 
 export const registerRequest = async (email: string, name: string, last_name:string, password: string) => {
-    axi.post("/users/register", {email, name, last_name, password})
+    axi.post("/users/register/", {email, name, last_name, password})
 }
