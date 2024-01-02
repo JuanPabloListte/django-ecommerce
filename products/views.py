6744e36d5b9a45b09498d54d05d2abd505d70de6
Juan Pablo Listte <juanpilistte@gmail.com>
2024-01-02 20:23:16 -0300

    .

--- products/views.py
+++ products/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from . models import Product
+from . serializers import ProductSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_product(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_product(request, name):
+    products = Product.objects.get(name=name)
+    serializer = ProductSerializer(products, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_product(request):
+    if request.user.is_staff:
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            Response(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['PUT'])
+def edit_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    if request.user.is_staff:
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            Response(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['DELETE'])
+def delete_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    if request.user.is_staff:
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
